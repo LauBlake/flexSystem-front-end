23ed312e6fb7c793f8df3b41ae732d835fc38bd4
S4muFD3v <samufdev64@gmail.com>
2025-11-10 20:46:28 -0300

    More supply functionality

--- FlexSystem-Front-End/src/Features/Supplies/services/supplyService.ts
+++ FlexSystem-Front-End/src/Features/Supplies/services/supplyService.ts
@@ -1,9 +1,15 @@
 import { apiClient } from "../../service/api.ts";
 import type { SupplyData } from "../supply.interface.ts";
 
+interface SupplyQuery {
+    supplyType?: string; 
+}
 
-
-
+const parseSearch = (query: SupplyQuery) => {
+    let result : string = "";
+    if(query.supplyType) result = result.concat("supplyType=" + query.supplyType);
+    return result;
+}
 
 export const supplyService = {
     async getSupply(id: number, supplyType: string): Promise<SupplyData> {
@@ -11,6 +17,17 @@ export const supplyService = {
             const response = await apiClient.get(`orders/supplies/${supplyType}/${id}`);
             console.log("Supply data: " + response.data);
             return response.data as SupplyData;
+        } catch (error)
+        {
+            throw new Error('Error consulting orders: ' + (error as Error).message);
+        }
+    },
+
+    async searchSupplies(search: SupplyQuery) {
+        try {
+            const response = await apiClient.get(`orders/supplies/search-client?${parseSearch(search)}`);
+            console.log("Supply data: " + response.data);
+            return response.data as SupplyData[];
         } catch (error)
         {
             throw new Error('Error consulting orders: ' + (error as Error).message);
