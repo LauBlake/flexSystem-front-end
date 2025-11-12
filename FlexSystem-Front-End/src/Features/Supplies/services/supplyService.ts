0cd87a0b6f9bebceced47aeaeea38a528e15f7ec
S4muFD3v <samufdev64@gmail.com>
2025-11-12 18:58:23 -0300

    Merge mistake fixed

--- FlexSystem-Front-End/src/Features/Supplies/services/supplyService.ts
+++ FlexSystem-Front-End/src/Features/Supplies/services/supplyService.ts
@@ -1,13 +1,33 @@
 import { apiClient } from "../../service/api.ts";
 import type { SupplyData } from "../supply.interface.ts";
-import type { SupplyKind } from "../../Pedido/order.interface.ts";
+
+interface SupplyQuery {
+    supplyType?: string; 
+}
+
+const parseSearch = (query: SupplyQuery) => {
+    let result : string = "";
+    if(query.supplyType) result = result.concat("supplyType=" + query.supplyType);
+    return result;
+}
 
 export const supplyService = {
-    async getSupply(id: number, supplyType: SupplyKind): Promise<SupplyData> {
+    async getSupply(id: number, supplyType: string): Promise<SupplyData> {
         try {
             const response = await apiClient.get(`orders/supplies/${supplyType}/${id}`);
             console.log("Supply data: " + response.data);
             return response.data as SupplyData;
+        } catch (error)
+        {
+            throw new Error('Error consulting orders: ' + (error as Error).message);
+        }
+    },
+
+    async searchSupplies(search: SupplyQuery) {
+        try {
+            const response = await apiClient.get(`orders/supplies/search-client?${parseSearch(search)}`);
+            console.log("Supply data: " + response.data);
+            return response.data as SupplyData[];
         } catch (error)
         {
             throw new Error('Error consulting orders: ' + (error as Error).message);
