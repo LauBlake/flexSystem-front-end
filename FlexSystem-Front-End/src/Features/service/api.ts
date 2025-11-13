e3f2f89a4917c8e8c39e5400d30f7048415584ce
S4muFD3v <samufdev64@gmail.com>
2025-11-13 18:57:13 -0300

    Fixed post token not sending

--- FlexSystem-Front-End/src/Features/service/api.ts
+++ FlexSystem-Front-End/src/Features/service/api.ts
@@ -3,17 +3,19 @@ const API_BASE_URL = 'http://localhost:3000';
 export const apiClient = {
     async post (endpoint: string, data: any) {
         try{
+            const token = localStorage.getItem('authToken');
             const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    ...(token? {Authorization: `Bearer ${token}`} : {})
                 },
                 body: JSON.stringify(data),
             });
             const responseData = await response.json();
             if (!response.ok) {
                 throw new Error(responseData.message || 'Error en la solicitud');
-            }
+            } 
             return responseData;
         }catch (error) {
             console.error('Error en la solicitud POST:', error);
@@ -42,6 +44,28 @@ export const apiClient = {
             return responseData;
         }catch (error) {
             console.error('Error en la solicitud GET:', error);
+            throw error;
+        }
+    },
+
+    async patch(endpoint: string, data: any) {
+        try{
+            const token = localStorage.getItem('authToken');
+            const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...(token? {Authorization: `Bearer ${token}`} : {})
+                },
+                body: JSON.stringify(data),
+            });
+            const responseData = await response.json();
+            if (!response.ok) {
+                throw new Error(responseData.message || 'Error en la solicitud');
+            } 
+            return responseData;
+        }catch (error) {
+            console.error('Error en la solicitud POST:', error);
             throw error;
         }
     }
